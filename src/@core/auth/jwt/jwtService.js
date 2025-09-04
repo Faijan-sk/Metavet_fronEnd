607f8763dbf9e01f35e8c5a0c5425fc6aa4699a2
Faijan-sk <shaikhfaizan.jb@gmail.com>
2025-09-04 16:17:01 +0530

    port added

--- src/@core/auth/jwt/jwtService.js
+++ src/@core/auth/jwt/jwtService.js
@@ -1,161 +1,175 @@
-  import axios from 'axios'
+import axios from 'axios'
 import jwtDefaultConfig from './jwtDefaultConfig'
 
-  //  axios.defaults.baseURL = 'http://192.168.29.200:8010/'
+// PRODUCTION GCP Configuration - PORT 8080 add kiya gaya hai
+// axios.defaults.baseURL = 'http://192.168.29.200:8010/'
+axios.defaults.baseURL = 'http://34.61.254.251:8080/'
 
-  axios.defaults.baseURL = 'http://34.61.254.251/'
+// Optional: Environment-based configuration
+// const API_BASE_URL = process.env.NODE_ENV === 'production' 
+//   ? 'http://34.61.254.251:8080/' 
+//   : 'http://localhost:8080/';
+// axios.defaults.baseURL = API_BASE_URL;
 
-  export default class JwtService {
-    // ** jwtConfig <= Will be used by this service
-    jwtConfig = { ...jwtDefaultConfig }
-    // ** For Refreshing Token
-    isAlreadyFetchingAccessToken = false
-    // ** For Refreshing Token
-    subscribers = []
-    constructor(jwtOverrideConfig) {
-      this.jwtConfig = { ...this.jwtConfig, ...jwtOverrideConfig }
+export default class JwtService {
+  // ** jwtConfig <= Will be used by this service
+  jwtConfig = { ...jwtDefaultConfig }
+  // ** For Refreshing Token
+  isAlreadyFetchingAccessToken = false
+  // ** For Refreshing Token
+  subscribers = []
+  
+  constructor(jwtOverrideConfig) {
+    this.jwtConfig = { ...this.jwtConfig, ...jwtOverrideConfig }
 
-      // ** Request Interceptor
-      axios.interceptors.request.use(
-        (config) => {
-           
-          // ** Get token from localStorage
-          const accessToken = this.getToken()
+    // ** Request Interceptor
+    axios.interceptors.request.use(
+      (config) => {
+        console.log('Making request to:', config.url);
+        
+        // ** Get token from localStorage
+        const accessToken = this.getToken()
 
-          // ** If token is present add it to request's Authorization Header
-          if (accessToken) {
-            // ** eslint-disable-next-line no-param-reassign
-            config.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
-          }
-          return config
-        },
-        (error) => Promise.reject(error)
-      )
-
-      // ** Add request/response interceptor
-      axios.interceptors.response.use(
-        (response) => response,
-        (error) => {
-           
-          // ** const { config, response: { status } } = error
-          const { config, response } = error
-          const originalRequest = config
-
-          // ** if (status === 401) {
-          // if (response && response.status === 401) {
-          //   if (window.location.pathname !== '/admin-panel/login') {
-          //     localStorage.removeItem('userData')
-          //     window.location.href = '/admin-panel/login'
-          //     return Promise.reject(error)
-          //   }
-          //   if (!this.isAlreadyFetchingAccessToken) {
-          //     this.isAlreadyFetchingAccessToken = true
-          //     this.refreshToken().then((r) => {
-          //       this.isAlreadyFetchingAccessToken = false
-          //       // ** Update accessToken in localStorage
-          //       this.setToken(r.data.accessToken)
-          //       this.setRefreshToken(r.data.refreshToken)
-          //       this.onAccessTokenFetched(r.data.accessToken)
-          //     })
-          //   }
-          //   const retryOriginalRequest = new Promise((resolve) => {
-          //     this.addSubscriber((accessToken) => {
-          //       // ** Make sure to assign accessToken according to your response.
-          //       // ** Check: https://pixinvent.ticksy.com/ticket/2413870
-          //       // ** Change Authorization header
-          //       originalRequest.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
-          //       resolve(this.axios(originalRequest))
-          //     })
-          //   })
-          //   return retryOriginalRequest
-          // }
-          return Promise.reject(error)
+        // ** If token is present add it to request's Authorization Header
+        if (accessToken) {
+          // ** eslint-disable-next-line no-param-reassign
+          config.headers.Authorization = `${this.jwtConfig.tokenType} ${accessToken}`
+          console.log('Added Authorization header');
+        } else {
+          console.log('No access token found');
         }
-      )
-    }
+        
+        // Add common headers for CORS
+        config.headers['Content-Type'] = 'application/json';
+        config.headers['Access-Control-Allow-Origin'] = '*';
+        
+        return config
+      },
+      (error) => {
+        console.error('Request interceptor error:', error);
+        return Promise.reject(error);
+      }
+    )
 
-    onAccessTokenFetched(accessToken) {
-      this.subscribers = this.subscribers.filter((callback) =>
-        callback(accessToken)
-      )
-    }
+    // ** Add request/response interceptor
+    axios.interceptors.response.use(
+      (response) => {
+        console.log('Response received:', response.status);
+        return response;
+      },
+      (error) => {
+        console.error('Response error:', error.response?.status, error.response?.data);
+        
+        const { config, response } = error
+        const originalRequest = config
 
-    addSubscriber(callback) {
-      this.subscribers.push(callback)
-    }
+        // Handle 401 Unauthorized responses
+        if (response && response.status === 401) {
+          console.log('Unauthorized access - redirecting to login');
+          // Optional: Clear token and redirect
+          // localStorage.removeItem('accessToken');
+          // localStorage.removeItem('userData');
+          // window.location.href = '/login';
+        }
+        
+        // Handle network errors
+        if (!response) {
+          console.error('Network error - check if backend is running');
+        }
+        
+        return Promise.reject(error)
+      }
+    )
+  }
 
-    getToken() {
-      return localStorage.getItem(this.jwtConfig.storageTokenKeyName)
-    }
+  onAccessTokenFetched(accessToken) {
+    this.subscribers = this.subscribers.filter((callback) =>
+      callback(accessToken)
+    )
+  }
 
-    getRefreshToken() {
-      return localStorage.getItem(this.jwtConfig.storageRefreshTokenKeyName)
-    }
+  addSubscriber(callback) {
+    this.subscribers.push(callback)
+  }
 
-    setToken(value) {
-      localStorage.setItem(this.jwtConfig.storageTokenKeyName, value)
-    }
+  getToken() {
+    return localStorage.getItem(this.jwtConfig.storageTokenKeyName)
+  }
 
-    setRefreshToken(value) {
-      localStorage.setItem(this.jwtConfig.storageRefreshTokenKeyName, value)
-    }
+  getRefreshToken() {
+    return localStorage.getItem(this.jwtConfig.storageRefreshTokenKeyName)
+  }
 
-    refreshToken() {
-      return axios.post(this.jwtConfig.refreshEndpoint, {
-        refreshToken: this.getRefreshToken(),
-      })
-    }
+  setToken(value) {
+    localStorage.setItem(this.jwtConfig.storageTokenKeyName, value)
+  }
 
-/*
-*     User Services
-*/
+  setRefreshToken(value) {
+    localStorage.setItem(this.jwtConfig.storageRefreshTokenKeyName, value)
+  }
 
-     register(...args) {
-      return axios.post(this.jwtConfig.registerEndpoint, ...args)
-    }
+  refreshToken() {
+    return axios.post(this.jwtConfig.refreshEndpoint, {
+      refreshToken: this.getRefreshToken(),
+    })
+  }
 
-     verifyOtp(otpData, token) {
+  /*
+  *     User Services
+  */
+  register(...args) {
+    console.log('Calling register API');
+    return axios.post(this.jwtConfig.registerEndpoint, ...args)
+  }
+
+  verifyOtp(otpData, token) {
+    console.log('Calling verify OTP API');
     // Use the token passed as parameter (from Redux)
     return axios.post(`${this.jwtConfig.otpVerifyEndPoint}/${token}`, otpData);
-    }
+  }
 
-     login(...args) {
-      return axios.post(this.jwtConfig.loginEndpoint, ...args)
-    }
+  login(...args) {
+    console.log('Calling login API');
+    return axios.post(this.jwtConfig.loginEndpoint, ...args)
+  }
 
-
-   /*
+  /*
   *   Doctor Services
   */
-createDoctor(payload) {
-  return axios.post(this.jwtConfig.updateDpctorEndPoint, payload, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("accessToken")}`, // adjust if you store token differently
-      "Content-Type": "application/json",
-    },
-  });
-}
+  createDoctor(payload) {
+    console.log('Calling create doctor API');
+    return axios.post(this.jwtConfig.updateDpctorEndPoint, payload, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        "Content-Type": "application/json",
+      },
+    });
+  }
 
   getAllDoctors() {
-     
-  return axios.get(this.jwtConfig.getAllDoctorEndPoint);
-}
+    console.log('Calling get all doctors API');
+    return axios.get(this.jwtConfig.getAllDoctorEndPoint);
+  }
 
   getDoctorById(doctorId) {
+    console.log('Calling get doctor by ID API');
     return axios.get(`${this.jwtConfig.getDoctorByIdEndPoint}/${doctorId}`)
   }
 
   createPets(...args) {
-    return axios.post(this.jwtConfig.createPets,...args)
+    console.log('Calling create pets API');
+    return axios.post(this.jwtConfig.createPets, ...args)
   }
   
   getAllPets(userId) {
+    console.log('Calling get all pets API');
     return axios.get(`${this.jwtConfig.getAllPets}${userId}`)
   }
 
   getUserByMobile(mobileNumber) {
-  return axios.get(`${this.jwtConfig.getUserByMobileEndPoint}`, {
-    params: { phoneNumber: mobileNumber }, // matches @RequestParam
-  });
-}
+    console.log('Calling get user by mobile API');
+    return axios.get(`${this.jwtConfig.getUserByMobileEndPoint}`, {
+      params: { phoneNumber: mobileNumber }, // matches @RequestParam
+    });
   }
+}
